3399c70be2b3c9888ec7237a8a7b68540918e450
Charlie Enriquez-Sarano <tennis.sarano@gmail.com>
2022-01-23 17:43:57 -0600

    adding alamofire pod and new workspace file to work with cocoapods

--- Dispo Take Home/Util/GifApiClient.swift
+++ Dispo Take Home/Util/GifApiClient.swift
@@ -1,5 +1,27 @@
 import UIKit
+import Alamofire
 
 struct GifAPIClient {
   // TODO: Implement
+}
+
+class GIFNetworkLayer {
+    
+    static let shared = GIFNetworkLayer()
+    
+    private init() {
+        // not sure if we need anything in the initializer - maybe a singleton isn't the best
+    }
+    
+    func getGifById(id: String) {
+        
+    }
+    
+    func getTrendingGifs() {
+        
+    }
+    
+    func searchGifByTerm(term: String) {
+        
+    }
 }
